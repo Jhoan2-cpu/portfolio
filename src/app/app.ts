fff08b7f914a94bae798eb0a26c7c8220b80cdf5
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2026-01-19 13:47:37 -0500

    feat: add admin page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { AboutComponent } from './components/about/about.component';
-import { EducationComponent } from './components/education/education.component';
-import { SoftSkillsComponent } from './components/soft-skills/soft-skills.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { VideoSectionComponent } from './components/video-section/video-section.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HeaderComponent,
-    HeroComponent,
-    AboutComponent,
-    EducationComponent,
-    SoftSkillsComponent,
-    ProjectsComponent,
-    VideoSectionComponent
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
