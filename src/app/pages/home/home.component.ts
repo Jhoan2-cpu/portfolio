b2a8df5d024d3be7f0285c802511d7328042ca44
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2026-01-21 13:44:13 -0500

    fix: build

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,9 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HeroComponent } from '../../components/hero/hero.component';
-import { AboutComponent } from '../../components/about/about.component';
 import { EducationComponent } from '../../components/education/education.component';
 import { SoftSkillsComponent } from '../../components/soft-skills/soft-skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
-import { VideoSectionComponent } from '../../components/video-section/video-section.component';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +13,9 @@ import { VideoSectionComponent } from '../../components/video-section/video-sect
     CommonModule,
     HeaderComponent,
     HeroComponent,
-    AboutComponent,
     EducationComponent,
     SoftSkillsComponent,
-    ProjectsComponent,
-    VideoSectionComponent
+    ProjectsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
